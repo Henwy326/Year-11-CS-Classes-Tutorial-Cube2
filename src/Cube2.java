2a84dc4f01831d5b3fdd7dfaa29eb2ad041759f1
Henwy326 <henrysr2028@student.cis.edu.hk>
2025-11-18 12:52:47 +0800

    init
    Added color attribute and methods for Cube2 class.

--- src/Cube2.java
+++ src/Cube2.java
@@ -12,18 +12,63 @@ public class Cube2 {
         basicCube.setSide(side);
     }
 
+    public Cube2(int side, String color) {
+        this(side);
+        this.color = color;
+    }
+
     public int getSide() {
         return basicCube.getSide();
     }
 
-    //the following methods have been started for you, but need to be completed
-    public int calculateVolume() { return 0;}
-    public int calculateSurfaceArea() { return 0;}
-    public Cube2 add(Cube2 otherCube) { return new Cube2(); }
-    public Cube2 minus(Cube2 otherCube) { return new Cube2(); }
-    public boolean equals(Cube2 otherCube) { return false; }
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setSide(int side){
+        basicCube.setSide(side);
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public String toString() {
-        return basicCube.toString();
+        return "Cube{side=" + basicCube.getSide() + ", color=\"" + this.color + "\"}";
+    }
+
+    //the following methods have been started for you, but need to be completed
+    public int calculateVolume() { 
+        return basicCube.calculateVolume();
+    }
+
+    public int calculateSurfaceArea() { 
+        return basicCube.calculateSurfaceArea();
+    }
+
+    public Cube2 add(Cube2 otherCube) {
+        // calculation: √(a² + b²)
+        int newSide = (int) Math.sqrt((otherCube.getSide() * otherCube.getSide()) + (this.getSide() * this.getSide()));
+        
+        // Check if the calculation is valid (perfect square)
+        int expectedSquare = (otherCube.getSide() * otherCube.getSide()) + (this.getSide() * this.getSide());
+        if (newSide * newSide != expectedSquare) {
+            throw new IllegalArgumentException("The resulting side length is not an integer");
+        }
+        
+        return new Cube2(newSide, this.color);
+    }
+
+    public Cube2 minus(Cube2 otherCube) { 
+        int newSide = (int) Math.sqrt((otherCube.getSide() * otherCube.getSide()) - (this.getSide() * this.getSide()));
+
+        return new Cube2(newSide, this.color);
+    }
+
+    public boolean equals(Cube2 otherCube) { 
+        if (otherCube.getSide() == this.getSide() && otherCube.getColor() == color){
+            return true;
+        }; 
+        return false;
     }
 }
