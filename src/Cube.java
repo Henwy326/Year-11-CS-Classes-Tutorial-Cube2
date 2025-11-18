1860879a01d41606bda7e522f776cc2bfcf9f55f
Henwy326 <henrysr2028@student.cis.edu.hk>
2025-11-18 13:11:39 +0800

    Final

---
+++ src/Cube.java
@@ -0,0 +1,39 @@
+public class Cube {
+    // properties
+    private int side;
+
+    // constructors
+    public Cube(int side){
+        setSide(side);
+    }
+    public Cube(){
+        this.side = 1;
+    }
+
+    // getter
+    public int getSide(){
+        return side;
+    }
+
+    // setter
+    public void setSide(int side){
+        if (side < 1) {
+            throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
+        }
+        this.side = side;
+    }
+
+    // functions 
+    public int calculateVolume(){
+        return side*side*side;
+    }
+
+    public int calculateSurfaceArea(){
+        return 6 * side*side;
+    }
+
+    // toString
+    public String toString(){
+        return "Cube{Side=" + side + "}";
+    }
+}
